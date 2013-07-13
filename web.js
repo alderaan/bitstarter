6f5ac76d77ba3a41a65f0680e97e7d1017efebe1
David <jollyneuron@gmail.com>
2013-07-13 22:43:45 +0000

    now reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var readtobuf = fs.readFileSync('index.html');
+var buftostring = readtobuf.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buftostring);
 });
 
 var port = process.env.PORT || 5000;
